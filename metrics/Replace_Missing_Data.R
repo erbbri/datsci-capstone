61939849c53b496d50a9d0ab99f20f5698726549
Bridget <erbb@spu.edu>
2022-05-20 04:07:45 -0700

    Create Replace_Missing_Data.R

---
+++ metrics/Replace_Missing_Data.R
@@ -0,0 +1,12 @@
+library(dplyr)
+library(tidyr)
+four_year <- read_csv("CollegeData_4-year.csv")
+replace_missing <- CollegeData_4_year %>%
+  mutate_at(vars(col_white, mkt_white, dif_white,
+                 col_hispa, mkt_hispa, dif_hispa,
+                 col_black, mkt_black, dif_black,
+                 col_asian, mkt_asian, dif_asian,
+                 col_amind, mkt_amind, dif_amind,
+                 col_pacis, mkt_pacis, dif_pacis,
+                 col_twora, mkt_twora, dif_twora), 
+            ~replace_na(.,mean(., na.rm = TRUE)))
